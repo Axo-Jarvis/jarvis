350d7f4a09752ded1d7f50773041132c115aebd6
Sung Ku Kang <sungkuka@gmail.com>
2023-11-04 12:19:56 -0700

    completed pie chart and status bar

---
+++ client/components/chartColors.js
@@ -0,0 +1,13 @@
+const BACKGROUND_COLOR = [
+  'rgba(255, 175, 205, 0.3)',
+  'rgba(205, 180, 220, 0.3)',
+  'rgba(160, 210, 255, 0.3)',
+];
+
+const BORDER_COLOR = [
+  'rgba(255, 175, 205, 1)',
+  'rgba(205, 180, 220, 1)',
+  'rgba(160, 210, 255, 1)',
+];
+
+export { BACKGROUND_COLOR, BORDER_COLOR };
