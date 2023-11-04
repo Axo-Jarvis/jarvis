4420fdf3fa8df967dc7c819037d5a3b17cbe199d
Nancy Huang <nancyhuang@nancyhmac.lan>
2023-11-04 14:07:23 -0700

    nancy task first commit

---
+++ client/components/App.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+import TaskContainer from './task-container.jsx';
+
+const App = () => {
+  return <div>Hello
+    <TaskContainer />
+  </div>;
+};
+
+export default App;
