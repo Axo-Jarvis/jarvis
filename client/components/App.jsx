cf936ba78bdc9f9b78445c7298e43261ba5620c5
Patrick <lepcwdetw@gmail.com>
2023-11-06 13:14:10 -0800

    pwang040: merged dev-branch into calendar

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,6 +3,8 @@ import BasicDateCalendar, * as calendar from './calendar.jsx';
 import { createRoot } from 'react-dom/client';
 import TaskCard from './taskCard.jsx';
 import TaskList from './TaskList.jsx';
+import AuthPage from './AuthPage.jsx';
+import Home from './home-page.jsx';
 import '/styles/container.scss';
 
 const App = () => {
@@ -10,8 +12,15 @@ const App = () => {
     setSelectedDate(date);
   };
   const [selectedDate, setSelectedDate] = useState(new Date());
+  const [isLogin, setIsLogin] = useState(false);
+  const [username, setUsername] = useState(null);
+  const changeLogin = username => {
+    setIsLogin(true);
+    setUsername(username);
+  };
   return (
     <div className='container'>
+          <Home currentUser={'test20'} />
     <BasicDateCalendar onDateChange={handleDateChange} />
     <TaskList selectedDate={selectedDate}/> <br/>
     <TaskCard selectedDate={selectedDate}/><br/>
@@ -20,30 +29,4 @@ const App = () => {
 };
 const root = createRoot(document.querySelector('#root'));
 root.render(<App />);
-export default App;
-//if aria-selectd is true then display task associated with that 
-//conditionally render a card component of some sortimport React, { useEffect, useState } from 'react';
-import AuthPage from './AuthPage.jsx';
-import Home from './home-page.jsx';
-
-const App = () => {
-  const [isLogin, setIsLogin] = useState(false);
-  const [username, setUsername] = useState(null);
-
-  const changeLogin = username => {
-    setIsLogin(true);
-    setUsername(username);
-  };
-
-  //   return (
-  //     <div>{isLogin ? <Home currentUser={userName}/> : <AuthPage changeLogin={changeLogin} />}</div>
-  //   );
-  // };
-
-  return (
-    // HARDCODED USERNAME
-    <Home currentUser={'test20'} />
-  );
-};
-
 export default App;
